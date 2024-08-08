ae34d1d3263efdeddd08daeb7698855cfafe5dde
Chris Horne <chrhorne@deloitte.com>
2024-08-08 16:27:21 -0700

    add babel.config.js & jest.config.js to frontend directory

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy"
+  },
+  testEnvironment: "jsdom"
+};
